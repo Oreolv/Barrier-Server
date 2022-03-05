4009d4d48ae0965db0303b1ad322539450cb74a5
“Oreolv” <455001000@qq.com>
2022-03-05 20:46:39 +0800

    ci: 修改MySQL配置文件

--- config/db.js
+++ config/db.js
@@ -1,10 +1,22 @@
-const MYSQL_CONF = {
-  host: 'localhost',
-  port: '3306',
-  database: 'barrier',
-  user: 'root',
-  password: '123456',
-};
+let MYSQL_CONF;
+
+if (process.env.NODE_ENV == 'production') {
+  MYSQL_CONF = {
+    host: '127.0.0.1',
+    port: '3306',
+    database: 'barrier',
+    user: 'barrier',
+    password: 'F5ewL6LRJRZZ6Hsz',
+  };
+} else {
+  MYSQL_CONF = {
+    host: 'localhost',
+    port: '3306',
+    database: 'barrier',
+    user: 'root',
+    password: '123456',
+  };
+}
 
 module.exports = {
   MYSQL_CONF,
